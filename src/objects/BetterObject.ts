2ad27d762ad5b3c786c2e83bac30e600b7b51c29
SMJSGaming <sjoerd.t@live.nl>
2021-12-02 12:59:15 +0100

    Implemented exact entries

--- src/objects/BetterObject.ts
+++ src/objects/BetterObject.ts
@@ -1,14 +1,15 @@
 import { DynamicObject } from "../interfaces/DynamicObject";
 import { entryOf } from "../types/entryOf";
+import { exactEntryOf } from "../types/exactEntryOf";
 import { keyList } from "../types/keyList";
 
 export class BetterObject<T> implements DynamicObject<T> {
 
     public static assign<T, U>(target: T, ...sources: U[]): T & U {
         // Tricking the compiler because merging 2 types by reference isn't truly possible
-        sources.forEach((source) => Object.entries(source).forEach(([key, value]) => target[key as keyof T] = value));
+        sources.forEach((source) => Object.entries(source).forEach(([key, value]) => target[<keyof T>key] = value));
 
-        return target as T & U;
+        return <T & U>target;
     }
 
     public static merge<T>(target: T, ...sources: T[]): T {
@@ -33,7 +34,7 @@ export class BetterObject<T> implements DynamicObject<T> {
         }));
 
         if (Array.isArray(target)) {
-            keysRemaining.reverse().forEach((key) => target.splice(key as number, 1));
+            keysRemaining.reverse().forEach((key) => target.splice(<number>key, 1));
         } else {
             keysRemaining.forEach((key) => delete target[key]);
         }
@@ -46,7 +47,7 @@ export class BetterObject<T> implements DynamicObject<T> {
     }
     
     public static keys<T>(object: T): keyList<T> {
-        return Object.keys(object) as keyList<T>;
+        return <keyList<T>>Object.keys(object);
     }
 
     public static values<T>(object: T): entryOf<T>[] {
@@ -54,7 +55,7 @@ export class BetterObject<T> implements DynamicObject<T> {
     }
 
     public static entries<T>(object: T): [keyof T, entryOf<T>][] {
-        return BetterObject.keys(object).map((key) => [key as keyof T, object[key as keyof T]]);
+        return BetterObject.keys(object).map((key) => [<keyof T>key, object[<keyof T>key]]);
     }
 
     public static fromEntries<T>(entries: [keyof T, entryOf<T>][]): T {
@@ -62,14 +63,14 @@ export class BetterObject<T> implements DynamicObject<T> {
             target[key] = value;
 
             return target;
-        }, {} as T);
+        }, <T>{});
     }
 
-    public static getProperty<T, U extends entryOf<T>>(object: T, key: keyof T): U {
-        return object[key] as U;
+    public static getProperty<T, K extends keyof T>(object: T, key: K): exactEntryOf<T, K> {
+        return object[key];
     }
 
-    public static setProperty<T, U extends entryOf<T>>(object: T, key: keyof T, value: U): T {
+    public static setProperty<T, K extends keyof T>(object: T, key: K, value: exactEntryOf<T, K>): T {
         object[key] = value;
 
         return object;
